599049dffb3084c3fc05eab75289d8034c8d0a4f
Tsuyoposon <sinngo_band@yahoo.co.jp>
2017-11-30 22:00:50 +0900

    [add]wireコマンド

--- source4/make_command.cpp
+++ source4/make_command.cpp
@@ -8,9 +8,12 @@ using namespace std;
 std::string create_rand_str();
 void create_test_data();
 void read_file_command(std::string file_path);
+void read_file_command2(std::string file_path);
 std::string digitString(int num, int digit);
 
 int main() {
+
+  create_test_data();
 
   read_file_command("./source3/test_data_files/");
 
@@ -36,7 +39,12 @@ std::string create_rand_str(){
   std::string str(256, '1');
   std::bitset<8> bs_char;
   for(int i=0;i<256;i++){
-    str[i] = rand() % 255 + 1;
+    int ran = rand() % 220 + 35;
+      if (ran == 92 || ran == 36){
+        ran = 90;
+      }
+    str[i] = ran;
+
     // bs_char(static_cast<u_int>(str[i]))
     // std::cout << "rand_char:" << static_cast<int>(str[i]) << std::endl;
   }
@@ -52,6 +60,7 @@ void read_file_command(std::string file_path){
   dirent* entry; // readdir() で返されるエントリーポイント
   int i=0;
   std::string d_buf;
+  std::string all_footers("frame contains \"");
 
   dp = opendir(path);
   if (dp==NULL) exit(1);
@@ -73,12 +82,20 @@ void read_file_command(std::string file_path){
         std::string read_str(it, last);
         std::cout << "file_name:" << d_buf << std::endl;
         std::cout << "rad_str:" << read_str << std::endl;
+        all_footers = all_footers + read_str + "\" or frame contains \"";
         fs.close();
-
       }
     }
   } while (entry != NULL);
 
+  fstream write_wire;
+  write_wire.open("source4/command_wireshark2.txt", ios::out);
+  if(! write_wire.is_open()) {
+      return;
+  }
+
+  write_wire.write((const char*)&all_footers[0], all_footers.size());;
+  write_wire.close();
 }
 
 void create_test_data(){
@@ -97,17 +114,46 @@ void create_test_data(){
   }
 }
 
-std::string digitString(int num, int digit) {
-  char c[32];
-  sprintf(c, "%d", num);
-  std::string s(c);
+void read_file_command2(std::string file_path){
+  const char* path = file_path.c_str();
+  DIR *dp;       // ディレクトリへのポインタ
+  dirent* entry; // readdir() で返されるエントリーポイント
+  int i=0;
+  std::string d_buf;
+  std::string all_footers("frame contains \"");
 
-  if(s.length() > digit) {
-    s = s.substr(s.length()-digit);
-  } else if(s.length() < digit){
-    while (s.length() < digit) {
-      s = "0" + s;
+  dp = opendir(path);
+  if (dp==NULL) exit(1);
+  do {
+    entry = readdir(dp);
+    if (entry != NULL){
+      // std::cout << path << entry->d_name << std::endl;
+      d_buf = std::string(path) + std::string(entry->d_name);
+      i++;
+      if (2<i){
+        ifstream fs;
+        fs.open(d_buf, ios::in);
+        if(! fs.is_open()) {
+          return;
+        }
+        fs.seekg(-16, ios_base::end);
+        std::istreambuf_iterator<char> it(fs);
+        std::istreambuf_iterator<char> last;
+        std::string read_str(it, last);
+        std::cout << "file_name:" << d_buf << std::endl;
+        std::cout << "rad_str:" << read_str << std::endl;
+        all_footers = all_footers + read_str + "\" or frame contains \"";
+        fs.close();
+      }
     }
+  } while (entry != NULL);
+
+  fstream write_wire;
+  write_wire.open("source4/command_wireshark2.txt", ios::out);
+  if(! write_wire.is_open()) {
+      return;
   }
-  return s;
+
+  write_wire.write((const char*)&all_footers[0], all_footers.size());;
+  write_wire.close();
 }
